71216ce9bddb8e42e682212e2cab043d11957cb8
Juda Kaleta <juda.kaleta@gmail.com>
2012-06-30 22:22:59 +0200

    Settings

--- tchorici/tchorici/settings.py
+++ tchorici/tchorici/settings.py
@@ -4,21 +4,23 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-	# ('Your Name', 'your_email@example.com'),
+	('Juda Kaleta', 'juda.kaleta@gmail.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
 	'default': {
-		'ENGINE': 'django.db.backends.',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracl.
-		'NAME': '',						  # Or path to database file if using sqlite3.
-		'USER': '',						  # Not used with sqlite3.
-		'PASSWORD': '',					  # Not used with sqlite3.
-		'HOST': '',						  # Set to empty string for localhost. Not used with sqlite3.
-		'PORT': '',						  # Set to empty string for default. Not used with sqlite3.
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': '/home/yetty/Work/Development/Tchorici/tchorici/database.sqlite',
+		'USER': '',
+		'PASSWORD': '',
+		'HOST': '',
+		'PORT': '',
 	}
 }
+
+APPEND_SLASH = True
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -27,11 +29,11 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Prague'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'cs-cz'
 
 SITE_ID = 1
 
@@ -48,12 +50,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/yetty/Work/Development/Tchorici/media'
+MEDIA_ROOT = '/home/yetty/Work/Development/Tchorici/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -99,6 +101,7 @@ MIDDLEWARE_CLASSES = (
 	'django.middleware.csrf.CsrfViewMiddleware',
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
 	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 	# Uncomment the next line for simple clickjacking protection:
 	# 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -135,12 +138,15 @@ INSTALLED_APPS = (
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
 	# Uncomment the next line to enable the admin:
-	# 'django.contrib.admin',
+	'django.contrib.admin',
 	# Uncomment the next line to enable admin documentation:
 	# 'django.contrib.admindocs',
 	'compressor',
+	'django.contrib.flatpages',
 
 	'tchorici',
+	'people',
+	'events',
 )
 
 # A sample logging configuration. The only tangible logging
