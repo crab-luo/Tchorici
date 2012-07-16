f22da81f14259259316844341cb96be287e2a72a
Juda Kaleta <juda.kaleta@gmail.com>
2012-07-16 12:28:12 +0200

    Adding some requirements

--- tchorici/people/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-from people.models import Person
-
-
-def list(request):
-	people = Person.objects.all()
-	return render(request, 'people/list.html', {'people': people})
