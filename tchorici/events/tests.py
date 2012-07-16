49a44b21e1ebe555d41d020e301fb9e7d4daf33e
Juda Kaleta <juda.kaleta@gmail.com>
2012-07-16 14:59:38 +0200

    Test and fab

--- tchorici/events/tests.py
+++ tchorici/events/tests.py
@@ -7,56 +7,51 @@ Replace this with more appropriate tests for your application.
 
 from datetime import datetime
 
-from django.conf import settings
 from django.core.files import File
 from django.test import TestCase
 
 from events.models import Event
 
-DATA_PATH = settings.FIXTURE_DIRS[0]
-
 
 class EventsTestCase(TestCase):
-	fixtures = ['events.json']
+    fixtures = ['events.json']
 
-	def test_photo(self):
-		photo = open(DATA_PATH + 'event_photo.jpg')
-		event = Event(name='Test', photo=File(photo), date_start=datetime.now())
-		event.save()
+    def test_photo(self):
+        photo = open('events/fixtures/event_photo.jpg')
+        event = Event(name='Test', photo=File(photo), date_start=datetime.now())
+        event.save()
 
-		print event.photo.width
+        print event.photo.width
 
 
 class EventsViewsTestCase(TestCase):
-	fixtures = ['events.json']
+    fixtures = ['events.json']
 
-	def test_index(self):
-		resp = self.client.get('/')
-		self.assertEqual(resp.status_code, 200)
+    def test_index(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
 
-		events = resp.context['events']
-		for i in range(events.paginator.num_pages):
-			self.test_page(i + 1, 200)
+        events = resp.context['events']
+        for i in range(events.paginator.num_pages):
+            self.test_page(i + 1, 200)
 
-	def test_page(self, i=1, status=200):
-		page = self.client.get('/seznam/' + str(i) + '/')
-		self.assertTrue('events' in page.context)
-		self.assertEqual(page.status_code, status)
+    def test_page(self, i=1, status=200):
+        page = self.client.get('/seznam/' + str(i) + '/')
+        self.assertTrue('events' in page.context)
+        self.assertEqual(page.status_code, status)
 
-	def test_last_page(self):
-		first_page = self.client.get('/seznam/1/')
-		pages = first_page.context['events'].paginator.num_pages
+    def test_last_page(self):
+        first_page = self.client.get('/seznam/1/')
+        pages = first_page.context['events'].paginator.num_pages
 
-		last_page = self.client.get('/seznam/' + str(pages) + '/').flush()
-		next_page = self.client.get('/seznam/' + str(pages + 1) + '/').flush()
+        last_page = self.client.get('/seznam/' + str(pages) + '/').flush()
+        next_page = self.client.get('/seznam/' + str(pages + 1) + '/').flush()
 
-		self.assertEqual(last_page, next_page)
+        self.assertEqual(last_page, next_page)
 
-	def test_detail(self):
-		events = Event.objects.all()
+    def test_detail(self):
+        events = Event.objects.all()
 
-		for event in events:
-			detail = self.client.get('/akce/' + event.slug + '.html')
-			self.assertEqual(detail.status_code, 200)
-
-
+        for event in events:
+            detail = self.client.get('/akce/' + event.slug + '.html')
+            self.assertEqual(detail.status_code, 200)
--- tchorici/fabfile.py
+++ tchorici/fabfile.py
@@ -1,7 +1,23 @@
-from fabric.api import env, run, local
+from fabric.api import env, run, local, settings, abort, prefix
+from fabric.contrib.console import confirm
 
 env.hosts = ['glor.cz']
-env.cwd = '/home/yetty/projects/Tchorici/'
+env.cwd = '/home/yetty/projects/Tchorici/tchorici'
+
+
+def deploy():
+    with prefix('workon tchorici'):
+        run('git pull')
+
+        with settings(warn_only=True):
+            result = run('./manage.py test')
+        if result.failed and not confirm('Tests failed. Continue?'):
+            if confirm('Return to previous version?'):
+                run('git reset --hard @{1}')
+            abort('Aborting...')
+
+        run('./manage.py compress')
+        run('sudo service tchorici restart')
 
 
 def push():
