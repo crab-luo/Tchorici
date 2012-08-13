40b2f5d59a770a516cdbec16140dbab5f3f7a150
Juda Kaleta <juda.kaleta@gmail.com>
2012-08-13 11:19:28 +0200

    Textarea for description

--- tchorici/events/admin.py
+++ tchorici/events/admin.py
@@ -9,49 +9,58 @@ from django.contrib import admin
 
 from django import forms
 
+FIELD_DESCRIPTION = Event._meta.get_field('description')
+
 
 class EventForm(forms.ModelForm):
-	people = forms.ModelMultipleChoiceField(
-		Person.objects.all(),
-		widget=FilteredSelectMultiple("Ucastnici", False, attrs={'rows': '10'})
-	)
+    people = forms.ModelMultipleChoiceField(
+        Person.objects.all(),
+        widget=FilteredSelectMultiple('Ucastnici', False, attrs={'rows': '10'}),
+    )
 
-	def clean_photo(self):
-		from django.core.files.images import get_image_dimensions
-		p = get_image_dimensions(self.cleaned_data['photo'])
-		width, height = p
+    description = forms.CharField(
+        label=FIELD_DESCRIPTION.verbose_name,
+        help_text=FIELD_DESCRIPTION.help_text,
+        widget=forms.Textarea,
+        required=False,
+    )
 
-		if width / height != 4:
-			raise forms.ValidationError('Fotografie musí být v poměru 4:1')
+    def clean_photo(self):
+        from django.core.files.images import get_image_dimensions
+        p = get_image_dimensions(self.cleaned_data['photo'])
+        width, height = p
 
-		return self.cleaned_data['photo']
+        if width / height != 4:
+            raise forms.ValidationError('Fotografie musí být v poměru 4:1')
 
-	class Meta:
-		model = Event
+        return self.cleaned_data['photo']
+
+    class Meta:
+        model = Event
 
 
 class LinkInline(admin.TabularInline):
-	model = Link
-	extra = 3
+    model = Link
+    extra = 3
 
 
 class EventAdmin(admin.ModelAdmin):
-	fieldsets = [
-		('Základní popis', {'fields': ['name', 'description', 'photo']}),
-		('Datum akce', {'fields': ['date_start', 'date_end']}),
-		('Účastníci', {'fields': ['people']}),
-	]
+    fieldsets = [
+        ('Základní popis', {'fields': ['name', 'description', 'photo']}),
+        ('Datum akce', {'fields': ['date_start', 'date_end']}),
+        ('Účastníci', {'fields': ['people']}),
+    ]
 
-	form = EventForm
-	filter_horizontal = ('people',)
-	inlines = [LinkInline]
+    form = EventForm
+    filter_horizontal = ('people',)
+    inlines = [LinkInline]
 
-	list_display = ('name', 'description', 'date_start', 'admin_photo')
-	list_filter = ['date_start']
-	date_hierarchy = 'date_start'
+    list_display = ('name', 'description', 'date_start', 'admin_photo')
+    list_filter = ['date_start']
+    date_hierarchy = 'date_start'
 
-	save_on_top = True
-	search_fields = ['name', 'description']
+    save_on_top = True
+    search_fields = ['name', 'description']
 
 
 admin.site.register(Event, EventAdmin)
